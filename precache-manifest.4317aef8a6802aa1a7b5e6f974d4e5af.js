47005aff83d7a6a32838af83b5518b947a5291e7
Alexandr Kolisetsky <skolisetsky@yahoo.com>
2019-12-16 14:29:28 +0200

    Updates

---
+++ precache-manifest.4317aef8a6802aa1a7b5e6f974d4e5af.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4db758c6e487e20046bfa2a8f112888b",
+    "url": "/goit-react-hw-05-budget-planner/index.html"
+  },
+  {
+    "revision": "fca98d7317c46b705103",
+    "url": "/goit-react-hw-05-budget-planner/static/css/main.67e7994e.chunk.css"
+  },
+  {
+    "revision": "165d1c47e95fb93fe81c",
+    "url": "/goit-react-hw-05-budget-planner/static/js/2.9d813835.chunk.js"
+  },
+  {
+    "revision": "fca98d7317c46b705103",
+    "url": "/goit-react-hw-05-budget-planner/static/js/main.945d78d5.chunk.js"
+  },
+  {
+    "revision": "01e99cb9f9299fb1cbd1",
+    "url": "/goit-react-hw-05-budget-planner/static/js/runtime-main.bece1991.js"
+  }
+]);
